ecd5449d704ad01af1a8066ac2d369fc34de8fad
Aryan-Kumar-7 <rajaryankumar26april@gmail.com>
2024-05-18 11:15:14 +0530

    loginsignin

--- frontend/src/pages/LoginSignup.jsx
+++ frontend/src/pages/LoginSignup.jsx
@@ -1,35 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
 import s3 from '../Components/Assets/s3.png'
+import s1 from '../Components/Assets/s1.png'
 import "./CSS/LoginSignup.css"
 
 const LoginSignup = () => {
+
+    const [ls, setLs] = useState("login");
+
     return (
         <div className='container'>
-            <div className="login">
-                <form action="">
-                    <h1>Login</h1>
-                    <div className="input-fields">
-                        <input type="text" placeholder='Username' name='Username' />
-                    </div>
-                    <div className="input-fields">
-                        <input type="Password" placeholder='Password' name='Password' />
-                    </div>
-                    <p>Create an account? <span>Click Here</span></p>
-                    <button>Sign In</button>
-                </form>
-            </div>
+            {ls === "login" ? <div className="login-container">
+                <div className="login">
+                    <form action="">
+                        <h1>Login</h1>
+                        <div className="input-fields">
+                            <input type="text" placeholder='Username' name='Username' />
+                        </div>
+                        <div className="input-fields">
+                            <input type="Password" placeholder='Password' name='Password' />
+                        </div>
+                        <p>Create an account? <span onClick={()=>{setLs("signin")}}>Click Here</span></p>
+                        <button>Continue</button>
+                    </form>
+                </div>
 
-            <div className="signupimg-container">
-                <div className="signupimg">
-                    <div className="signupimg-right">
-                        <h1>Hello User!</h1>
-                        <p>Enter your personal details and starts your journey with us</p>
-                        <img src={s3} alt="" />
-                        <button>Sign Up</button>
+                <div className="signupimg-container">
+                    <div className="signupimg">
+                        <div className="signupimg-right">
+                            <h1>Hello User!</h1>
+                            <p>Enter your personal details and starts your journey with us</p>
+                            <img src={s3} alt="" />
+                            <button onClick={()=>{setLs("signin")}}>Sign Up</button>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </div> : <div className="signin-container">
+                <div className="signin">
+                    <form action="">
+                        <h1>Sign Up</h1>
+                        <div className="input-fields">
+                            <input type="text" placeholder='Username' name='Username' />
+                        </div>
+                        <div className="input-fields">
+                            <input type="email" placeholder='Email Address' name='Email Address' />
+                        </div>
+                        <div className="input-fields">
+                            <input type="Password" placeholder='Password' name='Password' />
+                        </div>
+                        <p>Already have an account? <span onClick={()=>{setLs("signin")}}>Click Here</span></p>
+                        <button>Continue</button>
+                    </form>
+                </div>
+
+                <div className="signupimg-container">
+                    <div className="signupimg">
+                        <div className="signupimg-right">
+                            <h1>Welcome Back</h1>
+                            <p>To keep connected with us please login with your personal info</p>
+                            <img src={s1} alt="" />
+                            <button onClick={()=>{setLs("login")}}>Log In</button>
+                        </div>
+                    </div>
+                </div>
+            </div>}
+
+            
         </div>
+
+
     )
 }
 
