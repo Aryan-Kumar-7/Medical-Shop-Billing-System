fac648408ab21c421ef06b56d96f83141b512b66
Aryan-Kumar-7 <rajaryankumar26april@gmail.com>
2024-05-20 15:53:42 +0530

    c

--- frontend/src/Components/Navbar/NavBar.jsx
+++ frontend/src/Components/Navbar/NavBar.jsx
@@ -1,24 +1,31 @@
 import React, { useState } from 'react';
-import './Navbar.css'; 
+import './Navbar.css';
 import logo from '../Assets/logo.png';
+import cart_icon from '../Assets/cart_icon.png'
 import { Link } from 'react-router-dom';
 
 const NavBar = () => {
-    const [menu, setMenu] = useState("");
+  const [menu, setMenu] = useState("");
+  // const {getTotalCartItems} = useContext(ShopContext);
+
   return (
     <div className='navbar'>
       <div className="navlogo">
         <img src={logo} alt="" />
       </div>
       <ul className='nav-menu'>
-        <li onClick={() =>{setMenu("home")}}><Link style={{textDecoration: 'none'}} to='/'>Home</Link> {menu === "home"?<hr />:<></>}</li>
+        <li onClick={() => { setMenu("home") }}><Link style={{ textDecoration: 'none' }} to='/'>Home</Link> {menu === "home" ? <hr /> : <></>}</li>
         {/* <li onClick={() =>{setMenu("Login")}}>Login {menu === "Login"?<hr />:<></>}</li> */}
-        <li onClick={() =>{setMenu("doctors")}}><Link style={{textDecoration: 'none'}} to='/doctors'>Doctors</Link> {menu === "doctors"?<hr />:<></>}</li>
-        <li onClick={() =>{setMenu("medicine")}}><Link style={{textDecoration: 'none'}} to='/medicine'>Medicine</Link> {menu === "medicine"?<hr />:<></>}</li>
-        <li onClick={() =>{setMenu("services")}}><Link style={{textDecoration: 'none'}} to='/services'>Services</Link> {menu === "services"?<hr />:<></>}</li>
+        <li onClick={() => { setMenu("doctors") }}><Link style={{ textDecoration: 'none' }} to='/doctors'>Doctors</Link> {menu === "doctors" ? <hr /> : <></>}</li>
+        <li onClick={() => { setMenu("medicine") }}><Link style={{ textDecoration: 'none' }} to='/medicine'>Medicine</Link> {menu === "medicine" ? <hr /> : <></>}</li>
+        <li onClick={() => { setMenu("services") }}><Link style={{ textDecoration: 'none' }} to='/services'>Services</Link> {menu === "services" ? <hr /> : <></>}</li>
       </ul>
       <div>
         <button className='helpline'>Helpline</button>
+      </div>
+      <div className="nav-login-cart">
+        <Link to='/cart'><img src={cart_icon} alt="" /></Link>
+        <div className='nav-cart-count'>0</div>
       </div>
     </div>
   )
---
+++ frontend/src/Context/ShopContext.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+// export const ShopContext = createContext(null);
+// const getDefaultCart = () => {
+//     let cart = {};
+//     for (let index = 0; index < 300+1; index++) {
+//         cart[index] = 0;
+//     }
+//     return cart;
+// }
+
+
+
+// export default ShopContext
